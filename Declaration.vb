fee358ca7dd8e493e848e46afdc1711995343245
nyellll <159218653+nyellll@users.noreply.github.com>
2024-06-10 07:43:00 +0800

    Add files via upload

---
+++ Declaration.vb
@@ -0,0 +1,11 @@
+﻿
+
+Imports System.Data.OleDb
+
+Module Connection
+    Public ds As New DataSet
+    Public cmd As New OleDbCommand
+    Public conn As New OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\Danial\source\repos\revis\Database21.mdb")
+    Public dr As OleDbDataReader
+    Public dt As DataTable
+End Module
